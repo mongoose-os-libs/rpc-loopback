71bc7d10cd0b9165455a1569725164138155ee75
Dmitry Frank <mail@dmitryfrank.com>
2017-06-12 12:58:11 +0300

    Factor out rpc-loopback
    PUBLISHED_FROM=a7e877b198429712cbaecaf549d0bd72d4ae5506

---
+++ src/mg_rpc_channel_loopback.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014-2016 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_COMMON_MG_RPC_MG_RPC_CHANNEL_LOOPBACK_H_
+#define CS_COMMON_MG_RPC_MG_RPC_CHANNEL_LOOPBACK_H_
+
+#include "common/mg_rpc/mg_rpc_channel.h"
+#include "mongoose/mongoose.h"
+
+#if MGOS_ENABLE_RPC
+
+/*
+ * Creates a new loopback channel. Should be called for each incoming loopback
+ * request; `nc` is an incoming connection.
+ */
+struct mg_rpc_channel *mg_rpc_channel_loopback(void);
+
+bool mgos_rpc_loopback_init(void);
+
+#endif /* MGOS_ENABLE_RPC */
+#endif /* CS_COMMON_MG_RPC_MG_RPC_CHANNEL_LOOPBACK_H_ */
